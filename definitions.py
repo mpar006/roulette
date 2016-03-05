dd94d25bfcdbe51a7f02294055a3c57cb613ebc0
Michael Parker <edward.parker92@gmail.com>
2016-03-05 14:02:20 +1100

    added bet and table class, and their unit tests

--- definitions.py
+++ definitions.py
@@ -10,6 +10,9 @@ class Outcome:
     def winAmount(self, amount):
         return self.odds * amount
 
+    def getName(self):
+        return self.name
+
     def __eq__(self, other):
         return self.name == other.name
 
@@ -23,7 +26,7 @@ class Outcome:
         return "{} ({}:1)".format(self.name, self.odds)
 
     def __repr__(self):
-        return "Outcome({}, {}".format(self.name, self.odds)
+        return "Outcome({}, {})".format(self.name, self.odds)
 
 # Associates a bin on a roulette wheel with it's possible outcomes
 # e.g. the '0' bin has outcomes of a straight bet on 0 or the five number 
@@ -38,9 +41,18 @@ class Wheel:
     def __init__(self):
         self.bins = tuple(Bin() for i in range(38))
         self.rng = random.Random()
+	# A mapping of the name of an outcome to the outcome itself
+	self.outcomeMap = dict()
 
     def addOutcome(self, number, outcome):
         self.bins[number].add(outcome)
+	self.outcomeMap[outcome.getName()] = outcome
+
+    def getOutcome(self, name):
+        ret = None
+        if name.lower() in self.outcomeMap:
+            ret = self.outcomeMap[name.lower()]
+        return ret
 
     def next(self):
         return self.rng.choice(self.bins)
@@ -50,21 +62,28 @@ class Wheel:
 
 class BinBuilder():
     def __init__(self):
-
+	self.thing = 1
+	
     def buildBins(self, wheel):
-        straightBet(wheel)
-        streetBet(wheel)
-        lineBet(wheel)
-        dozenBet(wheel)
+    	self.straightBet(wheel)
+	self.horizontalSplitBet(wheel)
+	self.verticalSplitBet(wheel)
+        self.streetBet(wheel)
+	self.cornerBet(wheel)
+        self.lineBet(wheel)
+        self.dozenBet(wheel)
+	self.columnBet(wheel)
+	#self.evenMoneyBet(Wheel)
 
     def straightBet(self, wheel):
+	o = Outcome("straight bet", 35)
         for n in range(38):
-            wheel.addOutcome(n, Outcome("straight bet", 35))
+            wheel.addOutcome(n, o)
     
     def horizontalSplitBet(self, wheel):
+        o = Outcome("split bet", 17)
         for r in range(11):
             n = 3 * r + 1
-            o = Outcome("split bet", 17)
             wheel.addOutcome(n, o)
             wheel.addOutcome(n + 1, o)
             n = 3 * r + 2
@@ -72,23 +91,23 @@ class BinBuilder():
             wheel.addOutcome(n + 1, o)
 
     def verticalSplitBet(self, wheel):
+        o = Outcome("split bet", 17)
         for n in range(1,34):
-            o = Outcome("split bet", 17)
             wheel.addOutcome(n, o)
             wheel.addOutcome(n + 3, o)
 
     def streetBet(self, wheel):
+        o = Outcome("street bet", 11)
         for r in range(11):
             n = 3 * r + 1
-            o = Outcome("street bet", 11)
             wheel.addOutcome(n, o)
             wheel.addOutcome(n + 1, o)
             wheel.addOutcome(n + 2, o)
 
     def cornerBet(self, wheel):
+        o = Outcome("corner bet", 8)
         for r in range(10):
             n = 3 * r + 1
-            o = Outcome("corner bet", 8)
             wheel.addOutcome(n, o)
             wheel.addOutcome(n + 1, o)
             wheel.addOutcome(n + 3, o)
@@ -101,9 +120,9 @@ class BinBuilder():
 
 
     def lineBet(self, wheel):
+        o = Outcome("line bet", 5)
         for r in range(10):
             n = 3 * r + 1
-            o = Outcome("line bet", 5)
             wheel.addOutcome(n, o)
             wheel.addOutcome(n + 1, o)
             wheel.addOutcome(n + 2, o)
@@ -112,51 +131,84 @@ class BinBuilder():
             wheel.addOutcome(n + 5, o)
 
     def dozenBet(self, wheel):
+        o = Outcome("dozen Bet", 2)
         for d in range(2):
-            o = Outcome("dozen Bet", 2)
             for m in range(11):
                 wheel.addOutcome(12 * d + m + 1, o)
 
     def columnBet(self, wheel):
-        for c in range(3)
-            o = Outcome("column bet", 2)
-            for r in range(11)
+        o = Outcome("column bet", 2)
+        for c in range(3):
+            for r in range(11):
                 wheel.addOutcome(3 * r + c + 1, o)
 
-    def evenMoneyBet(self, wheel):
-        red = Outcome("red", 1)
-        black = Outcome("black", 1)
-        even = Outcome("even", 1)
-        odd = Outcome("odd", 1)
-        high = Outcome("high", 1)
-        low = Outcome("low", 1)
-        if n <= 1 and n < 19:
-            wheel.addOutcome(n, low)
-        else:
-            wheel.addOutcome(n, high)
+    #def evenMoneyBet(self, wheel):
+    #    red = Outcome("red", 1)
+    #    black = Outcome("black", 1)
+    #    even = Outcome("even", 1)
+    #    odd = Outcome("odd", 1)
+    #    high = Outcome("high", 1)
+    #    low = Outcome("low", 1)
+    #    for n in range(1,38):
+    #        if n <= 1 and n < 19:
+    #            wheel.addOutcome(n, low)
+    #        else:
+    #            wheel.addOutcome(n, high)
 
-        if n % 2 == 0:
-            wheel.addOutcome(n, even)
-        else:
-            wheel.addOutcome(n, odd)
+    #        if n % 2 == 0:
+    #            wheel.addOutcome(n, even)
+    #        else:
+    #            wheel.addOutcome(n, odd)
 
-        if n in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
-            wheel.addOutcome(n, red)
-        else:
-            wheel.addOutcome(n, black)
+    #        if n in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
+    #            wheel.addOutcome(n, red)
+    #        else:
+    #            wheel.addOutcome(n, black)
 
 # Used by the player to place an ammount on an outcome
-class bets:
-    pass
+class Bet:
+    def __init__(self, amountBet, outcome):
+        self.amountBet = amountBet
+        self.outcome = outcome
+
+    def winAmount(self):
+        return self.outcome.winAmount(self.amountBet) + self.amountBet
+
+    def looseAmount(self):
+        return self.amountBet
+
+    def __str__(self):
+        return "{} on {}".format(self.amountBet, self.outcome)
+
+    def __repr__(self):
+        return "Bet({}, {})".format(self.amountBet, self.outcome)
 
 # A collection of bets placed on outcomes placed by player
-class table:
-    pass
+class Table:
+    def __init__(self):
+        self.bets = set()
+
+    def placeBet(self, bet):
+        self.bets.add(bet)
+
+    def __str__(self):
+        ret = "Table("
+        for b in self.bets:
+            ret = ret + str(b) + ", "
+        ret = ret + ")"
+        return ret
+
+    def __repr__(self):
+        ret = "Table("
+        for b in self.bets:
+            ret = ret + str(b) + ", "
+        ret = ret + ")"
+        return ret
 
 # Employes a strategy to place an ammount of money on an outcome
-class player:
+class Player:
     pass
 
 # Runs the game - asks for bets, spins wheel, informs of wins/loses
-class game:
+class Game:
     pass
--- test_definitions.py
+++ test_definitions.py
@@ -1,6 +1,9 @@
 from definitions import Outcome
 from definitions import Bin
 from definitions import Wheel
+from definitions import BinBuilder
+from definitions import Bet
+from definitions import Table
 import pytest
 
 # Fixtures for Outcome tests
@@ -75,3 +78,35 @@ def test_add(wheel_fixture):
 def test_next(wheel_fixture):
    assert len(wheel_fixture.w.next()) == 3
    assert len(wheel_fixture.w.next()) == 0
+
+def test_map():
+   w = Wheel()
+   b = BinBuilder()
+   b.buildBins(w)
+   assert w.getOutcome("straight bet") == Outcome("straight bet", 35)
+   assert w.getOutcome("split bet") == Outcome("split bet", 17)
+   assert w.getOutcome("street bet") == Outcome("street bet", 11) 
+
+# commented out since this is a manual check that requires looking at stdout
+#def test_binBuild():
+#   w = Wheel()
+#   b = BinBuilder()
+#   b.buildBins(w)
+#   print w.get(1)
+
+def test_winAmount(outcome_fixture):
+   b = Bet(20, outcome_fixture.o1)
+   assert b.winAmount() == 720
+
+def test_looseAmount(outcome_fixture):
+   b = Bet(20, outcome_fixture.o1)
+   assert b.looseAmount() == 20
+
+def test_table(outcome_fixture):
+   t = Table()
+   b = Bet(20, outcome_fixture.o1)
+   t.placeBet(b)
+   b = Bet(50, outcome_fixture.o3)
+   t.placeBet(b)
+
+   print t
